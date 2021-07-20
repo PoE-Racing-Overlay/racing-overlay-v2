e6c4ec17d2c53a12b7893c1c807c24d285739d46
PX Slip <dev@pxslip.com>
2021-07-20 14:07:29 +0000

    Initial work moving overlay to Tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,25 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 fn main() {
   tauri::Builder::default()
+    .setup(move |app| {
+      let window = app.get_window("overlay").unwrap();
+      tauri::async_runtime::spawn(async move {
+        //TODO: The goal here is to set the size of windows based on the current monitor
+        // let monitor = window.primary_monitor().unwrap();
+        // match monitor {
+        //   Some(m) => {
+        //     let size = *m.size();
+        //     window.set_size(size);
+        //   }
+        //   None => println!("Error getting the primary monitor"),
+        // }
+      });
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
