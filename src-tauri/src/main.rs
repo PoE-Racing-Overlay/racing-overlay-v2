c3b33d32df73feb2ddbca926b86bd7e2b0d2ed26
PX Slip <dev@pxslip.com>
2021-08-06 00:51:02 +0000

    First shot at getting multiple overlay windows up

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,18 +8,25 @@ use tauri::Manager;
 fn main() {
   tauri::Builder::default()
     .setup(move |app| {
-      let window = app.get_window("overlay").unwrap();
-      tauri::async_runtime::spawn(async move {
-        //TODO: The goal here is to set the size of windows based on the current monitor
-        // let monitor = window.primary_monitor().unwrap();
-        // match monitor {
-        //   Some(m) => {
-        //     let size = *m.size();
-        //     window.set_size(size);
-        //   }
-        //   None => println!("Error getting the primary monitor"),
-        // }
-      });
+      let obj_opts = app.get_window("objectives");
+      match obj_opts {
+        Some(obj_window) => {
+          tauri::async_runtime::spawn(async move {
+            //TODO: The goal here is to set the size of windows based on the current monitor
+            let monitor = obj_window.primary_monitor();
+            match monitor {
+              Ok(monitor_opt) => match monitor_opt {
+                Some(m) => {
+                  // let size = *m.size();
+                }
+                None => {} // TODO: figure out what to do here
+              },
+              Err(e) => {} // TODO: figure out what I'm doing here as well
+            }
+          });
+        }
+        None => println!("Error getting the splash screen window"),
+      }
       Ok(())
     })
     .run(tauri::generate_context!())
